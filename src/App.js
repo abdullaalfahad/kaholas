951fee1f1d85d672aafe9e1ab8d07b83557eea84
abdullaalfahad <aafahad02@gmail.com>
2022-06-27 20:17:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto py-4'>
+      <h1 className='text-4xl font-medium'>Summer Art Camp! 5 Days of Artists Painting And Painting Monet, Van Gogh, Matisse & More</h1>
+      <p>Multi Day Course</p>
+      <div class="grid grid-cols-2 gap-4">
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
